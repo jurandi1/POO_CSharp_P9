18a1350c3e0c4e17b00216ee30d1ca053eadd6b0
Jurandi Junior <jurandijr20@gmail.com>
2025-01-08 22:30:44 -0300

    Teste inicial Matriz - Propriedade Length, Rank e GetLength

--- Program.cs
+++ Program.cs
@@ -4,6 +4,12 @@
     {
         private static void Main(string[] args)
         {
+            double[,] mat = new double[2, 3];
+
+            Console.WriteLine(mat.Length);
+            Console.WriteLine(mat.Rank);
+            Console.WriteLine(mat.GetLength(0));
+            Console.WriteLine(mat.GetLength(1));
         }
     }
 }
